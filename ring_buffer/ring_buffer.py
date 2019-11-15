dadd0ded67ad827bbf3e2e49740302b84165598e
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-15 14:58:03 -0600

    Finished Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,48 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    if self.current +1  == self.capacity:
+      self.current = 0
+    else:
+      self.current += 1
+    # if self.current == self.storage:
+    #   self.current = 0
+    # self.storage[0] = item
+    # self.current += 1
+
+    
 
   def get(self):
-    pass
+    # if self.storage:
+    #   return self.storage
+    ar= []
+    for i in range(self.capacity):
+      if self.storage[i] != None:
+        ar.append(self.storage[i])
+    return ar
+  
+
+  # class RingBuffer:
+	# def __init__(self,size_max):
+	# 	self.max = size_max
+	# 	self.data = []
+	# def append(self,x):
+	# 	"""append an element at the end of the buffer"""
+	# 	self.data.append(x)
+	# 	if len(self.data) == self.max:
+	# 		self.cur=0
+	# 		self.__class__ = RingBufferFull
+	# def get(self):
+  # 		""" return a list of elements from the oldest to the newest"""
+	# 	return self.data
+
+
+# class RingBufferFull:
+# 	def __init__(self,n):
+# 		raise "you should use RingBuffer"
+# 	def append(self,x):		
+# 		self.data[self.cur]=x
+# 		self.cur=(self.cur+1) % self.max
+# 	def get(self):
+# 		return self.data[self.cur:]+self.data[:self.cur]
